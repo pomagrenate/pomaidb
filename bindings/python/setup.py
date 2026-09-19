b4bcc74305a55f37ab19e3d99bbc4fc17acba920
pomegrante <vanhaminhquan2406@gmail.com>
2026-09-19 15:38:09 +0700

    fix(ci): bump bindings versions to 0.2.0 and update psync for Linux compilation

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,8 +1,8 @@
-﻿from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="pomaidb",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     package_data={
         "pomaidb": ["lib/*", "*.dll", "*.so", "*.dylib", "py.typed"],
